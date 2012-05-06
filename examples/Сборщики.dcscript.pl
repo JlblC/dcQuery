c75b6384216f4b8419becb82aff4f88d4138ef74
JlblC <jlblc@ya.ru>
2012-05-06 20:30:30 +0400

    Update examples/Сборщики.dcscript.pl

--- examples/Сборщики.dcscript.pl
+++ examples/Сборщики.dcscript.pl
@@ -8,7 +8,7 @@ foreach (@gathfleets)
     next unless $_;
     $units[0]->transferToAnotherFleet(_('id'));
     $Empire->writeVariable('ReturnFor'._('id'),_('x:y'));
-    _('JumpToNearestMy');
+    $_->jump(_($Empire->planets()->getNearestMy(_('x,y')),'x,y'));
   }
 my @carfleets = grep !_('turns_till_arrival'), $Empire->fleets()->getFleetsByName($car_name);
 foreach (@carfleets)
@@ -19,5 +19,3 @@ foreach (@carfleets)
     $box->unpack;
     $_->jump(split(/:/,_('ReturnFor'))); 
   }
-
-  
