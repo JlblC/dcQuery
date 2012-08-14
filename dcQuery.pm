a72c8d065c68eb025568cf02fbbe444c17fb7043
jlblc <jlblc@ya.ru>
2012-08-14 17:01:59 +0400

    add main file

---
+++ dcQuery.pm
@@ -0,0 +1,43 @@
+##### dcQuery v0.1.1
+sub _ {
+	local $_=$_;
+	my $arg;
+	foreach $arg (@_) {
+		$_=_1($arg);
+	}
+	return @{$_} if ref $_ eq "ARRAY";
+	return $_;
+
+	sub _1 {		
+		return _s($_[0]) if ref \$_[0] eq "SCALAR";
+		return &{$_[0]} if ref $_[0] eq "CODE";
+		return $_[0] if index(ref $_[0],"Safe::Hole::main")!=-1;
+		$Empire->log(3,"dcQuery ERROR type parametr type ". ref $_[0]);		
+	}
+
+	sub _s {
+		my @result;
+		if (index($_[0],',')!=-1){			
+			foreach $el (split /,/,$_[0]) {push @result,_s($el)};	
+			return \@result;
+		}
+		if (index($_[0],':')!=-1){			
+			foreach $el (split /:/,$_[0]) {push @result,_s($el)};	
+			return join ":",@result;	
+		}
+		if (index($_[0],'.')!=-1){			
+			foreach $el (split /\./,$_[0]) {push @result,_s($el)};	
+			return join "",@result;	
+		}
+
+		if (index($_[0],'^')!=-1){
+			my ($a,$b)=split /\^/,$_[0];
+			local $_=_s($a) if $a;
+			$b ? / \Q$b\E="([^\"]*)"/ : /<(\w+)\b/;
+			return $1;			
+		}
+		
+		return $_->getProp($_[0]) if defined $_->getProp($_[0]);
+		$Empire->log(3,"dcQuery ERROR parametr ".$_[0])
+	}
+}
